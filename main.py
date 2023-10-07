5d396438845aaf28724e903e0d9cd503648c2847
Geoffrey Martin <geoffm39@gmail.com>
2023-10-08 09:13:54 +1100

    fix player input bugs

--- main.py
+++ main.py
@@ -14,16 +14,19 @@ logo()
 print("Welcome to Tic-Tac-Toe!")
 print("Player 1 is 'X' and Player 2 is 'O'.")
 print("The starting player is random!")
-if input("1 player or 2 player game? (1/2)") == '1':
-    player1 = Player(input("Enter first player's name: "), False)
-    player2 = Player('CPU', True)
-else:
-    player1 = Player(input("Enter first player's name: "), False)
-    player2 = Player(input("enter second player's name: "), False)
-# todo: NEED RESPONSE AND LOOP HERE IF NOT 1 OR 2 SELECTED
-# TODO: I THINK MAYBE MAKE METHODS WITH RECURSION FOR THESE CHOICES (INCLUDING BELLOW)
-# TODO: PERHAPS SOME KIND OF TEMPLATE????
-
+while True:
+    user_input = input("1 player or 2 player game? (1/2)")
+    if user_input == '1':
+        player1 = Player(input("Enter first player's name: "), False)
+        player2 = Player('CPU', True)
+        break
+    elif user_input == '2':
+        player1 = Player(input("Enter first player's name: "), False)
+        player2 = Player(input("enter second player's name: "), False)
+        break
+    else:
+        clear_console()
+        print("Invalid input. Try again.")
 continue_playing = True
 while continue_playing:
     if random.randint(1, 2) == 1:
@@ -48,13 +51,12 @@ while continue_playing:
                         player2.increase_score()
                     print_board(board.array, player1, player2, winning_positions)
                     print(f"Player {current_player} has won the round!")
-                    making_choice = True
-                    while making_choice:
-                        another_game = input("Play another round? (y/n)").lower()
-                        if another_game == 'y':
+                    while True:
+                        user_input = input("Play another round? (y/n)").lower()
+                        if user_input == 'y':
                             board.clear_board()
-                            making_choice = False
-                        elif another_game == 'n':
+                            break
+                        elif user_input == 'n':
                             clear_console()
                             print(f"Final Scores:\nPlayer 1: {player1.score}\nPlayer 2: {player2.score}")
                             if player1.score > player2.score:
@@ -63,11 +65,11 @@ while continue_playing:
                                 print("Player 2 Wins!")
                             else:
                                 print("It was a draw!")
-                            making_choice = False
                             continue_playing = False
+                            break
                         else:
                             clear_console()
-                            print("Invalid response. Try again.")
+                            print("Invalid input. Try again.")
                 else:
                     print_board(board.array, player1, player2)
                     if current_player == 1:
